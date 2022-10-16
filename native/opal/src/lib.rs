d0cebe48c7bf2d73740a1c4e0c605cc16bba1e78
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-10-16 21:54:09 +0100

    docs: wip scope

--- native/opal/src/lib.rs
+++ native/opal/src/lib.rs
@@ -3,4 +3,21 @@ fn add(a: i64, b: i64) -> i64 {
     a + b
 }
 
+/*
+SCOPE -
+
+- getPCIPublicKey()
+- readKey({ armoredKey: atob(publicKey) })
+- createMessage({ text: JSON.stringify(dataToEncrypt) })
+- encrypt({
+    message,
+    encryptionKeys: decodedPublicKey,
+  }).then((ciphertext) => {
+    return {
+      encryptedMessage: btoa(ciphertext),
+      keyId,
+    }
+  })
+*/
+
 rustler::init!("Elixir.Opal", [add]);
