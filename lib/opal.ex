9d32ee3f3423d1a306b51e48e854f1a79b8844b4
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-10-16 23:53:17 +0100

    wip

--- lib/opal.ex
+++ lib/opal.ex
@@ -9,8 +9,8 @@ defmodule Opal do
   @spec get_pci_public_key() :: {:ok, Key.t()} | {:error, any()}
   def get_pci_public_key, do: error()
 
-  @spec encrypt(map()) :: {:ok, binary()} | {:error, any()}
-  def encrypt(_), do: error()
+  @spec encrypt(binary(), map()) :: {:ok, binary()} | {:error, any()}
+  def encrypt(_, _), do: error()
 
   defp error, do: :erlang.nif_error("not implemented")
 end
