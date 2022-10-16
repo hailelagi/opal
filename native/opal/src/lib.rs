b3956ef83c267f1e9fcef23dc7c60b5467886faa
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-10-16 22:04:57 +0100

    feat: get pci key

--- native/opal/src/lib.rs
+++ native/opal/src/lib.rs
@@ -1,12 +1,14 @@
-#[rustler::nif]
-fn add(a: i64, b: i64) -> i64 {
-    a + b
-}
+use sequoia_openpgp as openpgp;
+use openpgp::cert::prelude::*;
+
+use openpgp::packet::prelude::*;
+use sequoia_openpgp::parse::Parse;
+use openpgp::serialize::Serialize;
+use openpgp::types::Curve;
 
 /*
 SCOPE -
 
-- getPCIPublicKey()
 - readKey({ armoredKey: atob(publicKey) })
 - createMessage({ text: JSON.stringify(dataToEncrypt) })
 - encrypt({
@@ -19,5 +21,35 @@ SCOPE -
     }
   })
 */
+
+#[rustler::nif]
+fn add(a: i64, b: i64) -> i64 {
+    a + b
+}
+
+/* - getPCIPublicKey() */
+#[rustler::nif]
+fn get_pci_public_key() Key {
+  Key::from(Key4::generate_ecc(true, Curve::Ed25519)?);
+}
+
+// Generate a new certificate.  It has secret key material.
+let (cert, _) = CertBuilder::new()
+    .generate()?;
+
+let pk = cert.primary_key().key();
+assert!(pk.has_secret());
+
+// Serializing a `Key<key::PublicParts, _>` drops the secret key
+// material.
+let mut bytes = Vec::new();
+Packet::from(pk.clone()).serialize(&mut bytes);
+let p : Packet = Packet::from_bytes(&bytes)?;
+
+if let Packet::PublicKey(key) = p {
+    assert!(! key.has_secret());
+} else {
+    unreachable!();
+}
 
 rustler::init!("Elixir.Opal", [add]);
