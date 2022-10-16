7165696bdc219cd6ea7ed2c3513e1d1fb5266b5d
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-10-16 23:20:12 +0100

    chore: scope api

--- lib/opal.ex
+++ lib/opal.ex
@@ -4,7 +4,13 @@ defmodule Opal do
   """
   use Rustler, otp_app: :opal, crate: "opal"
 
+  alias Opal.Key
+
+  @spec get_pci_public_key() :: {:ok, Key.t()} | {:error, any()}
   def get_pci_public_key, do: error()
+
+  @spec encrypt(map()) :: {:ok, binary()} | {:error, any()}
+  def encrypt(_), do: error()
 
   defp error, do: :erlang.nif_error("not implemented")
 end
--- native/opal/src/lib.rs
+++ native/opal/src/lib.rs
@@ -16,29 +16,39 @@ pub struct OpalKey {
     pub public_key: String,
 }
 
-/*
-SCOPE -
-
-- readKey({ armoredKey: atob(publicKey) })
-- createMessage({ text: JSON.stringify(dataToEncrypt) })
-- encrypt({
-    message,
-    encryptionKeys: decodedPublicKey,
-  }).then((ciphertext) => {
-    return {
-      encryptedMessage: btoa(ciphertext),
-      keyId,
-    }
-  })
-*/
-
 /* - getPCIPublicKey() */
 #[rustler::nif]
 fn get_pci_public_key() -> NifResult<(Atom, OpalKey)> {
+  // todo: use match convert anyhow::Error -> rustler::Error
   let key = Key4::generate_ecc(true, Curve::Ed25519).unwrap();
   let key: Key<key::SecretParts, key::PrimaryRole> = Key::from(key);
 
   Ok((atoms::ok(), OpalKey{public_key: key.parts_into_public().to_string()}))
 }
 
+/*
+- encrypt({message, encryptionKeys: decodedPublicKey})
+*/
+/*
+#[rustler::nif]
+fn encrypt() {
+
+}
+
+*/
+
 rustler::init!("Elixir.Opal", [get_pci_public_key]);
+
+/* - readKey({ armoredKey: atob(publicKey) }) */
+// #[rustler::nif]
+// fn read_key() -> NifResult<(Atom, )> {
+
+// }
+
+/* - createMessage({ text: JSON.stringify(dataToEncrypt) }) */
+/*
+#[rustler::nif]
+fn create_message() {
+
+}
+*/
